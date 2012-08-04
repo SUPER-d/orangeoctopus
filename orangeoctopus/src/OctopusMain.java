f86c8a3e0da270680fadb282dd203a07bb712135
Svetlana <Svetlana@192.168.1.37>
2012-08-04 13:01:09 -0700

    Initial commit

---
+++ orangeoctopus/src/OctopusMain.java
@@ -0,0 +1,28 @@
+import org.newdawn.slick.*;
+
+public class OctopusMain extends BasicGame {
+
+	public OctopusMain(String title) {
+		super(title);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void render(GameContainer arg0, Graphics arg1) throws SlickException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void init(GameContainer arg0) throws SlickException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(GameContainer arg0, int arg1) throws SlickException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
